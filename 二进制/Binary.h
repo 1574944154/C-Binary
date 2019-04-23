67a7094eab1d460404e226272517e222e7d69ac3
1574944154 <a1574944154@gmail.com>
2019-04-23 22:30:46 +0800

    complete operator+ operator-

--- 二进制/Binary.h
+++ 二进制/Binary.h
@@ -4,15 +4,16 @@
 #include <string>
 using namespace std;
 
-#define BIT 8
+extern int SpecialArray[9];
 
 class Binary
 {
 public:
+	Binary(){}
 	Binary(int p[])
 	{
 		copy(binary, p);
-		sign = binary[1];
+		sign = binary[0];
 		absDecimalNum = binaryToAbsDecimal(binary);
 	}
 	Binary(int decimalNum)
@@ -46,6 +47,7 @@ public:
 		int tArray[9];
 		copy(tArray, convertBuMa().getBinary());
 		tArray[1] = 1 - sign;
+		tArray[0] = 1 - sign;
 		return Binary(tArray);
 	}
 	Binary convertBuMa()
@@ -53,8 +55,13 @@ public:
 		if (sign == 0)
 			return Binary(binary);
 		else if (sign == 1 && absDecimalNum == pow(2, 7))
-			return Binary(new int[9]{ 0,1,0,0,0,0,0,0,0 });
-		return Binary(add(convertFanMa().getBinary(), new int[9]{ 0,0,0,0,0,0,0,0,1 }));
+		{
+			return Binary(SpecialArray);
+		}
+		else if (arrayEqual(binary, SpecialArray))
+			return Binary(-pow(2, 7));
+		int tArray[9] = { 0,0,0,0,0,0,0,0,1 };
+		return Binary(add(convertFanMa().getBinary(), tArray));
 	}
 	Binary convertFanMa()
 	{
@@ -64,7 +71,7 @@ public:
 		int tArray[9];
 		for (int i = 8; i > 1; i--)
 			tArray[i] = 1 - binary[i];
-		tArray[1] = binary[1];
+		tArray[1] = binary[0];
 		tArray[0] = binary[0];
 		return Binary(tArray);
 	}
@@ -75,20 +82,28 @@ public:
 	Binary operator+(Binary b)
 	{
 		int *pt = add(convertBuMa().getBinary(), b.convertBuMa().getBinary());
+		if (*pt != *(pt + 1))
+		{
+			string error = *pt > 0 ? string("负溢") : string("正溢");
+			delete[] pt;
+			throw error;
+		}
 		Binary b1 = Binary(pt);
-		b1.display();
 		delete[] pt;
 		return b1.convertBuMa();
 	}
 	Binary operator-(Binary b)
 	{
 		int *pt = sub(convertBuMa().getBinary(), b.convertBuMa().getBinary());
+		if (*pt != *(pt + 1))
+		{
+			string error = *pt > 0 ? string("负溢") : string("正溢");
+			delete[] pt;
+			throw error;
+		}
 		Binary b1 = Binary(pt);
-		b1.display();
 		delete[] pt;
 		return b1.convertBuMa();
-		//return *this + Binary(-b.getDecimal());
-		//return Binary(1) + Binary(2);
 	}
 	string toBinaryString()
 	{
@@ -106,38 +121,41 @@ private:
 			sum += (p[i] * pow(2, 8 - i));
 		return sum;
 	}
+	bool arrayEqual(int x[], int y[])
+	{
+		for (int i = 0; i < 9; i++)
+			if (x[i] != y[i])
+				return false;
+		return true;
+	}
 	int *add(int x[], int y[])
 	{
 		int carry = 0;
 		int *tArray = new int[9];
-		for (int i = 8; i > 0; i--)
+		for (int i = 8; i >= 0; i--)
 		{
 			tArray[i] = x[i] + y[i] + carry;
 			carry = tArray[i] / 2;
 			tArray[i] = tArray[i] % 2;
 		}
-		tArray[0] = carry;
 		return tArray;
 	}
 	int *sub(int x[], int y[])
 	{
 		int borrow = 0;
 		int *tArray = new int[9];
-		for (int i = 8; i > 0; i--)
+		for (int i = 8; i >= 0; i--)
 		{
 			tArray[i] = x[i] - y[i] + borrow;
-			borrow = tArray[i] < 0 ? tArray[i] : 0;
-			tArray[i] = abs(tArray[i] % 2);
-			//borrow = tArray[i] < 0 ? -tArray[i] : 0;
-			//tArray[i] = tArray[i] >= 0 ? tArray[i] : -tArray[i];
+			borrow = tArray[i] < 0 ? -1 : 0;
+			tArray[i] = tArray[i] < 0 ? abs(tArray[i] % 2) : tArray[i];
 		}
-		tArray[0] = abs(borrow%2);
 		return tArray;
 	}
 	void init(int x)
 	{
 		binary[1] = sign;
-		binary[0] = 0;
+		binary[0] = sign;
 		for (int i = 8; i > 1; i--)
 		{
 			binary[i] = x % 2;
